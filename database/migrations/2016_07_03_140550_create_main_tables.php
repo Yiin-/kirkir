b8b951ecf30ecbe235affdff23a625c16e0755c5
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2016-07-14 02:00:36 +0300

    Daugiau informacijos apie augintinius duomenų bazėje

--- database/migrations/2016_07_03_140550_create_main_tables.php
+++ database/migrations/2016_07_03_140550_create_main_tables.php
@@ -51,28 +51,77 @@ class CreateMainTables extends Migration
             $table->timestamps();
         });
 
+        /**
+         * Visų augintinių lentelė
+         */
         Schema::create('pets', function (Blueprint $table) {
+            // Pagrindinis id šioje sistemoje
             $table->increments('id');
 
-            $table->string('name');
+            // Augintinio vardas
+            $table->string('name')->nullable();
 
+            // Vartotojo, kuris užregistravo šį augintinį id šioje sistemoje
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')
+            // Jeigu useris ištrinamas,
+            // pašalinam ir augintinį iš mūsų sistemos
                 ->onDelete('cascade');
 
+            // Augintinio tipas (katė, šuo, ?..)
             $table->integer('type_id')->unsigned()->nullable();
             $table->foreign('type_id')->references('id')->on('pet_types')
                 ->onDelete('set null');
 
+            // Veislės id šioje sistemoje
             $table->integer('breed_id')->unsigned()->nullable();
             $table->foreign('breed_id')->references('id')->on('breeds')
                 ->onDelete('set null');
 
+            // Ar augintinis yra patelė?
             $table->boolean('is_female');
-            $table->date('born_at');
-            $table->text('description');
-            $table->string('location');
 
+            // Gimimo data (MMMM-mm-dd)
+            $table->date('born_at')->nullable();
+
+            // Aprašymas (HTML)
+            $table->text('description');
+
+            // Augintinio lokacija
+            $table->string('location')->nullable();
+
+            // Statusas (veisimui, pardavimui, etc)
+            $table->text('status');
+
+            // Sąvininko vardas, pavardė
+            $table->string('owner')->nullable();
+
+            // Veisėjo vardas, pavardė arba pavadinimas
+            $table->string('breeder')->nullable();
+
+            // Veislyno pavadinimas
+            $table->string('cattery')->nullable();
+
+            // Klubo pavadinimas
+            $table->string('club')->nullable();
+
+            // Veislinio patino (tėvo) id šioje sistemoje
+            $table->integer('sire_id')->nullable();
+
+            // Veislinės patelės (tėvo) id šioje sistemoje
+            $table->integer('dam_id')->nullable();
+
+            /**
+             * top-cat.org informacija
+             */
+            // Augintinio profilio id
+            $table->integer('topcat_profile_id')->nullable();
+            // Veislinio patino (tėvo) id
+            $table->integer('sire_topcat_profile_id')->nullable();
+            // Veislinės patelės (motinos) id
+            $table->integer('dam_topcat_profile_id')->nullable();
+
+            // meta data
             $table->timestamps();
         });
 
